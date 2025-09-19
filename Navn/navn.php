8678117f04229de566fc1f8f2c851bd411e659aa
magnurdur <trevlandmagnus@gmail.com>
2025-09-19 10:33:41 +0200

    navn

---
+++ Navn/navn.php
@@ -0,0 +1,14 @@
+<?php
+
+function fulltNavn($fornavn,$etternavn)
+    
+    {$navn=$fornavn . " " . $etternavn;}
+
+    $fornavn=$_POST ["fornavn"];
+    $etternavn=$_POST ["etternavn"];
+
+    $navn=fulltNavn($fornavn,$etternavn);
+
+    print (Navnet ditt er $navn);
+    
+?>
